e22a8191428f18af833337215dd93911c8463e57
Alexandr <alex.mitin.psmu@gmail.com>
2020-01-08 13:54:41 +0500

    checkpoint

--- app/src/main/java/com/alexmitin/aboutme/MainActivity.kt
+++ app/src/main/java/com/alexmitin/aboutme/MainActivity.kt
@@ -2,11 +2,29 @@ package com.alexmitin.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        done_button.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editNicknameView = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editNicknameView.text
+        editNicknameView.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
     }
 }
